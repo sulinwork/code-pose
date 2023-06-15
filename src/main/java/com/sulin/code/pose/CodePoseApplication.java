2e0b0d69c98b5f681627b3f764879cfd43dfd0f5
sulin <sulinwork@163.com>
2023-06-15 17:07:39 +0800

    project init

---
+++ src/main/java/com/sulin/code/pose/CodePoseApplication.java
@@ -0,0 +1,13 @@
+package com.sulin.code.pose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePoseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodePoseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sulin/code/pose/CodePoseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulin.code.pose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePoseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
